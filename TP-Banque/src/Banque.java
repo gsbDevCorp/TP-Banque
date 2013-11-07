200d7c39eb06f2c9f4010381eeccb4a0e0c51ab5
Robin <rbilly.mail@gmail.com>
2013-11-07 20:52:13 +0100

    Modification de la classe Banque

--- TP-Banque/src/Banque.java
+++ TP-Banque/src/Banque.java
@@ -2,33 +2,62 @@ import java.util.ArrayList;
 
 public class Banque {
 	
-	private String client;
-	private int id;
-	
-	//arrayList de client
-	ArrayList<Client> clients = new ArrayList<Client>();
+//----- Attributs
+	ArrayList<Client> clients;
 
-	//constructeur vide
-	public Banque(){
-		
+//----- Constructeurs
+	public Banque(){ 
+		this.clients = new ArrayList<Client>();
+	}
 
+//----- Méthodes
+	/**
+	 * Fonction retournant le client le plus riche
+	 * de la banque, tous comptes confondus.
+	 * 
+	 * @return Client
+	 */
+	public Client plusRiche() {
+		double solde = 0.0;
+		Client clientPlusRiche;
+		for(Client client : this.clients) {
+			if (client.soldeGeneral() >= solde)
+				clientPlusRiche = client;
+		}
+		return clientPlusRiche;
 	}
 	
-	//constructeur avec tous les attributs
-	public Banque(String client, int id){
-		this.client = client;
-		this.id = id;
+	/**
+	 * Fonction retournant une liste de clients
+	 * ayant un solde négatif sur au moins un
+	 * compte.
+	 * 
+	 * @return ArrayList<Client>
+	 */
+	public ArrayList<Client> aDecouvert() {
+		ArrayList<Client> clientsDecouvert = new ArrayList<Client>();
+		boolean decouvert = false;
+		for(Client client : clients) {
+			for(Compte compte : client.comptes) {
+				if(compte.getSolde() < 0 )
+					decouvert = true;
+			}
+			if (decouvert == true) {
+				clientsDecouvert.add(client);
+				decouvert = false;
+			}
+		}
+		return clientsDecouvert;
 	}
-	//retourne le client le plus riche (tous comptes pris en compte)
 	
-	//retourne un ensemble de clients qui ont au moins un compte dans le rouge
-	
-	//ajoute un client
-	public void ajoute(Client client) {
-		clients.add(client);
-		
+	/**
+	 * Procédure d'ajout d'un client à la liste
+	 * des clients de la banque
+	 * 
+	 * @param Client
+	 * @return void
+	 */
+	public void ajoutClient(Client client) {
+		this.clients.add(client);
 	}
-	
-	
-
 }
