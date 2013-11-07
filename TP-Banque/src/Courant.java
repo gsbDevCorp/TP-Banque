a015f66e6a1ef86285f48eaf077e0ea493a503c7
Robin <rbilly.mail@gmail.com>
2013-11-07 19:19:07 +0100

    Modification de la classe Courant

--- TP-Banque/src/Courant.java
+++ TP-Banque/src/Courant.java
@@ -1,27 +1,110 @@
+/**
+ * 
+ * @author Robin Billy - SIO2
+ * 
+ * Classe compte courant
+ * 
+ * @extends Compte
+ */
+public class Courant extends Compte {
 
-public class Courant extends Compte{
+//----- Attributs
+	private double decouvert;
 	
-	//constructeur vide
-	public Courant() {
-		
-	}
+//----- Constructeurs
 	
-	//constructeur avec tous les attibuts
-	public Courant(String client, int id) {
+	/**
+	 * Constructeur vide
+	 */
+	public Courant() {}
+	
+	/**
+	 * Constructeur avec paramètres
+	 * 
+	 * @param client
+	 * @param id
+	 * @param decouvert
+	 */
+	public Courant(String client, int id, double solde, double decouvert) {
 		super(client, id);
-	}
-	
-	public boolean debite(double montant){
-		if(solde - montant < 0){
-			solde -= montant + (solde - montant);
-			return true;
-		}
-		else{
-			solde -= montant;
-			return true;
-		
-		}
-		
+		this.setDecouvert(decouvert);
 	}
 
+//----- Accesseurs
+	/**
+	 * Renvoie le découvert maximum
+	 * 
+	 * @return double
+	 */
+	public double getDecouvert() {
+		return this.decouvert;
+	}
+	
+//----- Modificateurs
+	/**
+	 * Modifie le découvert maximal
+	 * 
+	 * @param montant
+	 */
+	public void setDecouvert(double montant) {
+		this.decouvert = montant;
+	}
+	
+//----- Méthodes
+	/**
+	 * Procédure de créditation du solde.
+	 * La créditation ne s'effectue que si le montant
+	 * passé est positif.
+	 * 
+	 * @param montant
+	 */
+	public void crediter(double montant) {
+		if (isMontantPositif(montant))
+			super.setSolde(super.getSolde() + montant);
+	}
+	
+	/**
+	 * Fonction de débit du compte.
+	 * Le débit ne s'effectue que si le montant
+	 * passé est positif
+	 * 
+	 * @param montant
+	 * @return boolean
+	 */
+	public boolean debiter(double montant) {
+		if (isMontantPositif(montant)) {
+			if ((super.getSolde() - montant) < this.decouvert) {
+				super.setSolde(super.getSolde() - montant);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Fonction de transfert d'un montant du compte
+	 * vers un autre.
+	 * 
+	 * @param montant
+	 * @param numero
+	 * @return boolean
+	 */
+	public boolean transfertCompte(double montant, int numero) {
+		if (isMontantPositif(montant)) {
+			return super.transfertCompte(montant, numero);
+		}
+		return false;
+	}
+	
+	/**
+	 * Vérifie si un montant est positif
+	 * 
+	 * @param montant
+	 * @return boolean
+	 */
+	public boolean isMontantPositif(double montant) {
+		if (montant >= 0)
+			return true;
+		return false;
+	}
 }
